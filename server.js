c9e5a1bb99bdb57ce2c268e77a1e79786e971cca
alexgolubev <aleksander.golubev@gmail.com>
2014-05-22 22:53:09 +0200

    Using example from Heroku guidelines instead

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-var http = require('http');
-http.createServer(function handler(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(process.env.PORT || 1337, '127.0.0.1');
-console.log('Server running at ' + process.env.PORT || 1337);
+
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
