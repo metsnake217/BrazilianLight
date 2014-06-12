1712f03f0d91631a6096dff6429615278a333cae
metsnake217 <mkon@STO-MKON.sto.netlight.se>
2014-06-13 01:36:53 +0200

    scrolling adjustments

--- public/javascripts/scrolling.js
+++ public/javascripts/scrolling.js
@@ -8,7 +8,7 @@
           $target = $target.length && $target
           || $('[name=' + this.hash.slice(1) +']');
           if ($target.length) {
-            var targetOffset = $target.offset().top;
+            var targetOffset = $target.offset().top - 100;
             $('html,body')
             .animate({scrollTop: targetOffset}, 1000);
            return false;
