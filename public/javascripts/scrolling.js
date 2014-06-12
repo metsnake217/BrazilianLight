b3b33be2c12816515addece59b61b0f75b84bee4
metsnake217 <mkon@STO-MKON.sto.netlight.se>
2014-06-13 01:40:54 +0200

    better scrolling

--- public/javascripts/scrolling.js
+++ public/javascripts/scrolling.js
@@ -8,7 +8,7 @@
           $target = $target.length && $target
           || $('[name=' + this.hash.slice(1) +']');
           if ($target.length) {
-            var targetOffset = $target.offset().top - 100;
+            var targetOffset = $target.offset().top - 50;
             $('html,body')
             .animate({scrollTop: targetOffset}, 1000);
            return false;
